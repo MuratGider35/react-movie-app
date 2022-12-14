763a66bb337551fa2ea35e3735de133a347bc76a
MuratGider35 <m.gider35@gmail.com>
2022-12-14 22:37:03 +0300

    vercel.json

--- movie-app/src/pages/Home.jsx
+++ movie-app/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { createTheme, ThemeProvider } from "@mui/material";
 import MovieCard from "../components/MovieCard";
 import { toastWarnNotify } from "../helpers/ToastNotify";
 import { AuthContext } from "../context/AuthContextProvider";
-const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const API_KEY = "90dcd989e2559a744b7d261b77b74507";
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
@@ -55,34 +55,32 @@ const Home = () => {
     <ThemeProvider theme={theme}>
       <Navbar />
       <div className="homebg">
-      <form className="flex justify-center p-2" onSubmit={handleSubmit}>
-        <input
-          type="search"
-          className="w-80 h-8 rounded-md outline-none border p-1 m-2"
-          placeholder="Search a movie..."
-          onChange={(e) => setSearchTerm(e.target.value)}
-          value={searchTerm}
-        />
-        <button className="dark:text-white" type="submit">
-          Search
-        </button>
-      </form>
+        <form className="flex justify-center p-2" onSubmit={handleSubmit}>
+          <input
+            type="search"
+            className="w-80 h-8 rounded-md outline-none border p-1 m-2"
+            placeholder="Search a movie..."
+            onChange={(e) => setSearchTerm(e.target.value)}
+            value={searchTerm}
+          />
+          <button className="dark:text-white" type="submit">
+            Search
+          </button>
+        </form>
 
-      <div className="flex justify-center flex-wrap">
-        {loading ? (
-          <div
-            className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-blue-600 mt-52"
-            role="status"
-          >
-            <span className="visually-hidden">Loading...</span>
-          </div>
-        ) : (
-          movies.map((movie) => <MovieCard key={movie.id} {...movie} />)
-        )}
+        <div className="flex justify-center flex-wrap">
+          {loading ? (
+            <div
+              className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-blue-600 mt-52"
+              role="status"
+            >
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          ) : (
+            movies.map((movie) => <MovieCard key={movie.id} {...movie} />)
+          )}
+        </div>
       </div>
-      </div>
-
-   
     </ThemeProvider>
   );
 };
